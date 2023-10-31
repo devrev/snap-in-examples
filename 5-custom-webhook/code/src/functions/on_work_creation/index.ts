8d65da98b0dee36dc96eb62cdf6f73d4d18f8031
altafDevRev <altaf.ahmad@devrev.ai>
2023-10-31 15:40:44 +0530

    changed custom webhook

--- 5-custom-webhook/code/src/functions/on_work_creation/index.ts
+++ 5-custom-webhook/code/src/functions/on_work_creation/index.ts
@@ -2,43 +2,17 @@
  * Copyright (c) 2023 DevRev, Inc. All rights reserved.
  */
 
-import axios from 'axios';
-
-type HTTPResponse = {
-  success: boolean;
-  errMessage: string;
-  data: any;
-};
-async function postCallAPI(
-  endpoint: string,
-  payload: any,
-  authKey: string,
-) {
-  try {
-    const res: HTTPResponse = await axios.post(endpoint, payload, {
-      headers: {
-        Authorization: authKey,
-        'Content-type': 'application/json',
-      },
-    });
-    const data = res.data;
-    return { success: true, errMessage: 'Data successfully fetched', data: data };
-  } catch (error: any) {
-    if (error.response) {
-      return { success: false, errMessage: error.response.data, data: '' };
-    } else if (error.request) {
-      return { success: false, errMessage: error.request.data, data: '' };
-    } else {
-      return { success: false, errMessage: error, data: '' };
-    }
-  }
-}
+import { client } from "@devrev/typescript-sdk";
 
 async function handleEvent(
   event: any,
 ) {
   const devrevPAT = event.context.secrets.service_account_token;
   const API_BASE = event.execution_metadata.devrev_endpoint;
+  const devrevSDK = client.setup({
+    endpoint: API_BASE,
+    token: devrevPAT,
+  })
   const workCreated = event.payload.work_created;
   const bodyComment = event.payload.body;
   const body = {
@@ -46,7 +20,7 @@ async function handleEvent(
     type: 'timeline_comment',
     body: bodyComment,
   }
-  const response = await postCallAPI(API_BASE + '/timeline-entries.create', body, devrevPAT);
+  const response = await devrevSDK.timelineEntriesCreate(body as any);
   if (!response.success) {
     console.log(response.errMessage);
     return response;
