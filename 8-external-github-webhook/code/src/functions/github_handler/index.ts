06c49fc8917e733e98be2baa880c69babf54a2c8
Altaf Ahmad <104750669+altafDevRev@users.noreply.github.com>
2024-02-06 15:06:03 +0530

    enh: Add snap-in with external GitHub Integration (#12)
    * added snap-in
    
    * added secret and modified readme
    
    * check status code in event
    
    * renamed folder
    
    * made suggested changes and added type
    
    * nit: used type from SDK
    
    * Apply suggestions from code review
    
    Co-authored-by: Subhajyoti Ghosh <60965264+coder336@users.noreply.github.com>
    
    * nit: change in message
    
    ---------
    
    Co-authored-by: Subhajyoti Ghosh <60965264+coder336@users.noreply.github.com>

---
+++ 8-external-github-webhook/code/src/functions/github_handler/index.ts
@@ -0,0 +1,45 @@
+import { client, betaSDK } from '@devrev/typescript-sdk';
+
+// Handles the event from GitHub
+async function handleEvent(event: any) {
+  // Extract necessary information from the event
+  const token = event.context.secrets.service_account_token;
+  const endpoint = event.execution_metadata.devrev_endpoint;
+
+  // Set up the DevRev SDK with the extracted information
+  const devrevSDK = client.setup({
+    endpoint: endpoint,
+    token: token,
+  });
+
+  // Extract the part ID and commits from the event
+  const partID = event.input_data.global_values.part_id;
+  const commits = event.payload.commits;
+
+  // Iterate through commits and append the commit message to the body of the comment
+  let bodyComment = 'Commits from GitHub:\n';
+  for (const commit of commits) {
+    bodyComment += commit.message + '\n';
+  }
+
+  // Prepare the body for creating a timeline comment
+  const body: betaSDK.TimelineEntriesCreateRequest = {
+    body: bodyComment,
+    object: partID,
+    type: betaSDK.TimelineEntriesCreateRequestType.TimelineComment,
+  };
+
+  // Create a timeline comment using the DevRev SDK
+  const response = await devrevSDK.timelineEntriesCreate(body);
+
+  // Return the response from the DevRev API
+  return response;
+}
+
+export const run = async (events: any[]) => {
+  for (const event of events) {
+    await handleEvent(event);
+  }
+};
+
+export default run;
