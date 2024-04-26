364049c6e9b74175044dce7933d27595e19a054d
altafDevRev <altaf.ahmad@devrev.ai>
2024-04-27 00:36:32 +0530

    made suggested changes + working

--- 9-external-action/code/src/test-runner/example.test.ts
+++ 9-external-action/code/src/test-runner/example.test.ts
@@ -1,17 +1,55 @@
+import { FunctionInput, Context, ExecutionMetadata, InputData } from '@devrev/typescript-sdk';
 import { run } from '../functions/command_handler';
 
 describe('Test some function', () => {
-  it('Something', () => {
-    run([
-      {
-        payload: {
-          work_created: {
-            work: {
-              id: 'some-id',
-            },
-          },
-        },
+  const context: Context = {
+    dev_oid: 'devOrgId',
+    automation_id: 'automationId',
+    source_id: 'sourceId',
+    snap_in_id: 'snapInId',
+    snap_in_version_id: 'snapInVersionId',
+    service_account_id: 'serviceAccountId',
+    secrets: {
+      service_account_token: 'serviceAccountToken',
+      actor_session_token: 'actorSessionToken',
+    },
+  };
+
+  const executionMetadata: ExecutionMetadata = {
+    request_id: 'requestId',
+    function_name: 'functionName',
+    event_type: 'eventType',
+    devrev_endpoint: 'https://api.devrev.ai/',
+  }
+
+  const input_data: InputData = {
+    keyrings: {
+      github_connection: 'githubConnection',
+    },
+    global_values: {
+      globalValue: 'globalValue',
+    },
+    event_sources: {
+      eventSource: 'eventSource',
+    },
+  };
+
+  const event: FunctionInput = {
+    payload: {
+      orgName: 'testOrg',
+      repoName: 'testRepo',
+      issueDetails: {
+        title: 'testTitle',
+        description: 'testDescription',
+        issueDisplayName: 'testIssueDisplayName',
       },
-    ]);
+    },
+    context: context,
+    execution_metadata: executionMetadata,
+    input_data: input_data,
+  };
+
+  it('Something', () => {
+    run([event]);
   });
 });
