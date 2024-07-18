51a215a1946f4e261046130ab1dd0b66f14237ad
plaidbait91 <i-srikant.tangirala@devrev.ai>
2024-07-18 18:38:57 +0530

    save

--- 14-operations/code/src/functions/operation_handler/index.ts
+++ 14-operations/code/src/functions/operation_handler/index.ts
@@ -1,19 +1,15 @@
 import { OperationFactory } from '../../operations';
-import { ExecuteOperationInput,FunctionInput, OperationMap } from '@devrev/typescript-sdk/dist/snap-ins';
+import { ExecuteOperationInput, FunctionInput, OperationMap } from '@devrev/typescript-sdk/dist/snap-ins';
 
 // Operations
-import { GetTemperature } from './get_temperature';
-import { PostCommentOnTicket } from './post_comment_on_ticket';
-import { SendSlackMessage } from './send_slack_message';
+import { SendDevRevComment } from './send_devrev_comment';
 
 /**
  * Map of operations with the slug mentioned in the manifest.
  * The key is the slug of the operation mentioned in the manifest and value is the operation class.
  */
 const operationMap: OperationMap = {
-  get_temperature: GetTemperature,
-  post_comment_on_ticket: PostCommentOnTicket,
-  send_slack_message: SendSlackMessage,
+  send_devrev_comment: SendDevRevComment,
 };
 
 export const run = async (events: FunctionInput[]) => {
@@ -26,7 +22,7 @@ export const run = async (events: FunctionInput[]) => {
   const operationFactory = new OperationFactory(operationMap);
   const operation = operationFactory.getOperation(operationSlug, event);
   const ctx = operation.GetContext(event);
-  const resources = event.input_data.resources||{};
+  const resources = event.input_data.resources || {};
   return await operation.run(ctx, payload, resources);
 };
 
