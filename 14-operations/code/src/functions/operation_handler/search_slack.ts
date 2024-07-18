3925424aa176e988d17bd660ed76ddeba3a1fbb9
kaivu1999 <cs1170339@cse.iitd.ac.in>
2024-07-18 23:15:23 +0530

    Init slack search snap

--- 14-operations/code/src/functions/operation_handler/search_slack.ts
+++ 14-operations/code/src/functions/operation_handler/search_slack.ts
@@ -3,6 +3,7 @@ import { WebClient } from '@slack/web-api';
 
 interface SearchSlackInput {
   query: string;
+  channel_ids: string[];
 }
 
 export class SearchSlack extends OperationBase {
@@ -70,6 +71,7 @@ export class SearchSlack extends OperationBase {
     }
     
     const slackToken = resources.keyrings.slack_token.secret;
+    // Change the token here.
     let slackClient;
     try {
       slackClient = new WebClient(slackToken);
@@ -86,13 +88,39 @@ export class SearchSlack extends OperationBase {
       });
     }
 
+
+    var channel_names = [];
+    for (const channel_id in inputData.channel_ids) {
+      try {
+        const channelInfo = await slackClient.conversations.info({ channel: channel_id });
+        channel_names.push(channelInfo.channel?.name);
+      } catch (e: any) {
+        err = {
+          message: 'Error while fetching channel info:' + e.message,
+          type: Error_Type.InvalidRequest,
+        };
+        console.log(err);
+      }
+    }
+
     try {
-      const result = await slackClient.search.all({ query: query });
+      // Make in:channel_1 in_channel_2 in_channel_3 query
+      const channelString = channel_names.map((name) => `in:${name}`).join(' '); // Apply channel filter
+      const result = await slackClient.search.messages({ query: channelString + ' ' + query });
       const serializedResults = this.serializeSlackResults(result);
+      // Assuming serializedResults is an array of objects as described
+      var formattedMessages = [];
+
+      for (const result of serializedResults) {
+        // Template literal to format the string as required
+        const formattedMessage = `Message: ${result.text}  URL: ${result.url}`;
+        formattedMessages.push(formattedMessage);
+      }
+      console.log(formattedMessages)
       return OperationOutput.fromJSON({
         error: err,
         output: {
-          values: [{ results: serializedResults }],
+          values: [{ results: JSON.stringify(formattedMessages)}],
         } as OutputValue,
       });
     } catch (e: any) {
